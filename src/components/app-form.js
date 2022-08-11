9df2969551bf8042ccec3b9b898c2135e8411bfd
john-mirage <chaosdynamix@posteo.net>
2022-08-11 18:48:02 +0200

    feat: add form without states

--- src/components/app-form.js
+++ src/components/app-form.js
@@ -3,10 +3,13 @@ const template = document.getElementById("template-app-form");
 class AppForm extends HTMLElement {
   constructor() {
     super();
+    this.initialCall = true;
+    this.formElement = template.content.firstElementChild.cloneNode(true);
   }
 
   connectedCallback() {
     if (this.initialCall) {
+      this.append(this.formElement);
       this.initialCall = false;
     }
   }
